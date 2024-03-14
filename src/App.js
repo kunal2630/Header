09480a39cefddf1d6cc897f39981a1f2d3f45f89
kunal2630 <kunalk2630@gmail.com>
2024-03-14 14:34:58 +0530

    Revert "Login and Header page"
    This reverts commit a6b85a960765e07de9a5de5f1f3075e49e7728de.

--- src/App.js
+++ src/App.js
@@ -1,22 +1,14 @@
 import Header from "./Components/Header";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
-import Login from "./Components/Login";
-import Home from "./Components/Home";
-import About from "./Components/About";
-import Products from "./Components/Products";
+import { BrowserRouter } from "react-router-dom";
 
 function App() {
-	return (
-		<BrowserRouter>
-			<Header />
-			<Routes>
-				<Route path="/" element={<Login />} />
-				<Route path="/home" element={<Home />} />
-				<Route path="/about" element={<About />} />
-				<Route path="/products" element={<Products />} />
-			</Routes>
-		</BrowserRouter>
-	);
+  return (
+    <BrowserRouter>
+      <div className="App">
+        <Header />
+      </div>
+    </BrowserRouter>
+  );
 }
 
 export default App;
